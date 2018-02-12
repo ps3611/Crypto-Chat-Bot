7109988f49a630d12232fa7b425d8635a768d24d
Peter Tas Schindler <petertasschindler@Peters-MacBook-Pro.local>
2018-02-12 23:22:51 +0100

    good progress

--- js/script.js
+++ js/script.js
@@ -1,6 +1,3 @@
-let coins = {'BTC':'$11000','ETH':'$900','Bitcoin':'$11000','Ethereum':'$900',
-            'bitcoin':'$11000','ethereum':'$900','ether':'$900'}
-
 function addMessage(message,id) {
   let text = document.createTextNode(message);
   let para = document.createElement("P");
@@ -11,8 +8,8 @@ function addMessage(message,id) {
 }
 
 function updateScroll(){
-    let element = document.getElementById("chatbar");
-    element.scrollTop = element.scrollHeight;
+  let element = document.getElementById("chatbar");
+  element.scrollTop = element.scrollHeight;
 }
 
 function initialBotMessage() {
@@ -22,52 +19,58 @@ function initialBotMessage() {
 }
 
 function addUserMessage() {
-    let message = document.getElementsByTagName("input")[0].value;
-    addMessage(message,"user-text");
+  let message = document.getElementsByTagName("input")[0].value;
+  addMessage(message,"user-text");
 }
 
 function addBotAnswer() {
-    let question = document.getElementsByTagName("input")[0].value;
-    if(question[question.length-1] !== '?'){
-      setTimeout(addMessage,1000,'This is not a question!',"bot-text");
-      setTimeout(addMessage,2000,'Please ask me a question',"bot-text");
-    }
-    else {
-      fetchSymbol()
-    }
-    document.getElementById("messagebox").value = '';
+  let question = document.getElementsByTagName("input")[0].value;
+  if(question[question.length-1] !== '?'){
+    setTimeout(addMessage,1000,'This is not a question!',"bot-text");
+    setTimeout(addMessage,2000,'Please ask me a question',"bot-text");
+  }
+  else {
+    fetchSymbol(question);
+  }
+  document.getElementById("messagebox").value = '';
 }
 
-function fetchSymbol(){
+function fetchSymbol(question){
   fetch('https://min-api.cryptocompare.com/data/all/coinlist')
-    .then(
-      response => {
-        if (response.ok) return response.json();
-        throw new Error('Request failed!');
-      },
-      networkError => console.log(networkError.message)
-    )
-    .then(
-      jsonResponse => {
-        let symbol = Object.keys(jsonResponse['Data'])[40]
-        fetchPrice(symbol);
+  .then(
+    response => {
+      if (response.ok) return response.json();
+      throw new Error('Request failed!');
+    },
+    networkError => console.log(networkError.message)
+  )
+  .then(
+    jsonResponse => {
+      let all_symbols = Object.keys(jsonResponse['Data']);
+      let question_array = question.slice(0,-1).split(" ").map(el=>el.toUpperCase());
+      let symbols = [];
+      question_array.forEach(word => {if(all_symbols.includes(word)) symbols.push(word)});
+      symbols.forEach(symbol => {
+        setTimeout(fetchPrice,2000,symbol);
       }
     );
+  }
+);
 }
 
 function fetchPrice(symbol) {
   fetch('https://min-api.cryptocompare.com/data/price?fsym=' + symbol + '&tsyms=USD')
-    .then(
-      response => {
-        if (response.ok) return response.json();
-        throw new Error('Request failed!');
-      },
-      networkError => console.log(networkError.message)
-    )
-    .then(
-      jsonResponse => {
-        let answer = "The price of " + symbol + " is " + jsonResponse['USD'];
-        setTimeout(addMessage,1000,answer,"bot-text");
-      }
-    );
+  .then(
+    response => {
+      if (response.ok) return response.json();
+      throw new Error('Request failed!');
+    },
+    networkError => console.log(networkError.message)
+  )
+  .then(
+    jsonResponse => {
+      let answer = "The price of " + symbol + " is " + jsonResponse['USD'];
+      addMessage(answer,"bot-text");
+    }
+  );
 }
